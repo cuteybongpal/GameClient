2bbabc76fdc44d7b415a19de2fe3d140409bde62
gydnjs <khw33352182@gmail.com>
2025-04-14 12:01:53 +0900

    project : start

---
+++ TestClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace TestClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 0);
+
+            IPEndPoint connectAddr = new IPEndPoint(ipAddr, 7777);
+            Socket socket =  new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            socket.Bind(endPoint);
+            //socket.Connect(con)
+
+            while (true)
+            {
+
+            }
+        }
+    }
+}
